2add4c3d7505e4fd02174315046d40f51e05bb0f
Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>
2024-08-30 23:32:46 -0400

    CLI MVP in Rust

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,25 @@
-use std::env::args;
+use std::{
+    env::args,
+    fs::File,
+    io::{stdin, Write},
+};
 
 use anyhow::Result;
-use createnv::{model_to_text, parser, DEFAULT_ENV_SAMPLE};
+use parser::Parser;
+
+mod model;
+mod parser;
+
+const DEFAULT_ENV_SAMPLE: &str = ".env.sample";
+const DEFAULT_ENV: &str = ".env";
 
 fn main() -> Result<()> {
     let path = args()
         .nth(1)
         .unwrap_or_else(|| DEFAULT_ENV_SAMPLE.to_string());
-    if path == "--debug" {
-        model_to_text()?;
-        return Ok(());
-    }
-    parser(&path)?;
+    let mut parser = Parser::new(path.as_str())?;
+    parser.parse(&mut stdin().lock())?;
+    let mut output = File::create(DEFAULT_ENV)?;
+    output.write_all(parser.to_string().as_bytes())?;
     Ok(())
 }
